8ae92478fe69200b10e67cde768082a1691cb599
Ahmed Fady <ahmedfady426@gmail.com>
2026-05-18 15:41:08 +0300

    -- Add movie entity & Add app constants

---
+++ lib/core/constants/constants.dart
@@ -0,0 +1,4 @@
+class Constants {
+  static const String baseUrl = 'https://api.themoviedb.org/3';
+  static const String apiKey = "1b19c1640a7f2d9d17c6647308a2153f";
+}
---
+++ lib/movies/domain/entities/movie.dart
@@ -0,0 +1,29 @@
+import 'package:equatable/equatable.dart';
+
+class Movie extends Equatable {
+  final int id;
+  final String title;
+  final String backdropPath;
+  final List<int> generIds;
+  final String overview;
+  final double voteAverage;
+
+  const Movie({
+    required this.id,
+    required this.title,
+    required this.backdropPath,
+    required this.generIds,
+    required this.overview,
+    required this.voteAverage,
+  });
+
+  @override
+  List<Object?> get props => [
+    id,
+    title,
+    backdropPath,
+    generIds,
+    overview,
+    voteAverage,
+  ];
+}
